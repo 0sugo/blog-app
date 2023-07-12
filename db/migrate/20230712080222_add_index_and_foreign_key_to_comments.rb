39f8b5894ac55907a9d312dd2958f0efffc981e7
0sugo <josugo26@gmail.com>
2023-07-12 15:00:36 +0300

    Fix linters errors

--- db/migrate/20230712080222_add_index_and_foreign_key_to_comments.rb
+++ db/migrate/20230712080222_add_index_and_foreign_key_to_comments.rb
@@ -4,6 +4,5 @@ class AddIndexAndForeignKeyToComments < ActiveRecord::Migration[7.0]
     add_index :comments, :post_id
     add_foreign_key :comments, :users, column: :author_id
     add_foreign_key :comments, :posts, column: :post_id
-
   end
 end
--- db/migrate/20230712081556_add_foreign_key_and_foreign_key_to_likes.rb
+++ db/migrate/20230712081556_add_foreign_key_and_foreign_key_to_likes.rb
@@ -3,6 +3,6 @@ class AddForeignKeyAndForeignKeyToLikes < ActiveRecord::Migration[7.0]
     add_index :likes, :author_id
     add_index :likes, :post_id
     add_foreign_key :likes, :users, column: :author_id
-    add_foreign_key :likes, :posts,column: :post_id
+    add_foreign_key :likes, :posts, column: :post_id
   end
 end
