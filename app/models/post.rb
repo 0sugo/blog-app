593c44bf365f9ac4345685dd298521115de9848a
0sugo <josugo26@gmail.com>
2023-07-14 17:19:28 +0300

    Add posts validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, class_name: 'Like', dependent: :destroy
 
+  validates :title,presence: true,length: {maximum: 250}
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :lik
   after_save :update_posts
 
   def display_comments
