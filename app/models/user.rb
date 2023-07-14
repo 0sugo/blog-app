bda3b1c94b193042da03e6d7479f67e414cb5ccb
0sugo <josugo26@gmail.com>
2023-07-14 21:14:38 +0300

    Modify user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
-  validates :name,presence: true
-  validates :posts_counter,presence: true,numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
