ff7bcd78eeb83eef49d9946aadc67d6e5c3c2197
0sugo <josugo26@gmail.com>
2023-07-14 21:14:13 +0300

    Modify posts validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,16 +5,17 @@ class Post < ApplicationRecord
 
   validates :title,presence: true,length: {maximum: 250}
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :lik
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_posts
 
   def display_comments
-    comments.order(created_at: desc).limit(5)
+    comments.order(created_at: :desc).limit(5)
   end
 
   private
 
   def update_posts
-    author.increment(:posts_counter)
+    author.increment!(:posts_counter)
   end
 end
