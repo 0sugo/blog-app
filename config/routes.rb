e1869a52bbe29df5d3f960736fd81c1572b12612
0sugo <josugo26@gmail.com>
2023-07-18 17:38:21 +0300

    Fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "users#index"
+  root 'users#index'
 
-  resources :users,only: [:index, :show] do
-    resources :posts,only:[:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
-
 end
