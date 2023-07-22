acdefe2c90d37535fcae63578abb6f2d08f0bc3b
0sugo <josugo26@gmail.com>
2023-07-22 13:00:44 +0300

    Add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to user_post_path(current_user, @post), notice: 'Comment was successfully created.'
+    else
+      render 'posts/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
