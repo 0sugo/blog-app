9817b09e9aaea30bf883fb6950bfd4dacc4a5228
Stephen Katuli <stephenkatuli88@gmail.com>
2023-07-27 09:24:58 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
-
-  protected
-
-  def configure_permitted_parameters
-    attributes = [:name]
-    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
-    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  helper_method :current_user
+  def current_user
+    @current_user ||= User.first
   end
 end
